318c033fed4043e417dd33f035ad7bbf447c95bb
madmaxfarm105-hue <madmaxfarm105@gmail.com>
2026-03-27 18:01:32 -0300

    Atualizar o User.js

--- models/User.js
+++ models/User.js
@@ -1,20 +1,22 @@
 const mongoose = require('mongoose');
 
 const playerSchema = new mongoose.Schema({
-    googleId: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
-    name: { type: String, required: true },
-    avatar: { type: String },
-    level: { type: Number, default: 1 },
-    hp: { type: Number, default: 100 },
-    money: { type: Number, default: 0 },
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now }
+  googleId: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  avatar: { type: String, default: '' },
+
+  level: { type: Number, default: 1 },
+  hp: { type: Number, default: 100 },
+  money: { type: Number, default: 0 },
+
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
 });
 
-playerSchema.pre('save', function(next) {
-    this.updatedAt = Date.now();
-    next();
+playerSchema.pre('save', function (next) {
+  this.updatedAt = Date.now();
+  next();
 });
 
 const Player = mongoose.model('Player', playerSchema);
